18b77a2468626a0c26c3a030d8d88c0d62569ca2
mohamed elkaouay <122555850+mohamed-elko@users.noreply.github.com>
2023-10-07 16:47:39 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,341 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='page'>
+      <div className="header">
+        <img src="./public/link--logopng@2x.png" alt="" />
+        <div className="nav-links">
+          <ul>
+            <li><a href="#">+ home</a></li>
+            <li><a href="#">+ about</a></li>
+            <li><a href="#">+ Services</a></li>
+            <li><a href="#">+ Shop</a></li>
+            <li><a href="#">+ Blog</a></li>
+            <li><a href="#">+ Pages</a></li>
+            <li><a href="#">+ Contact</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="section1">
+        <div className="left-sec">
+          <img src="./public/layerpng@2x.png" alt="" />
+          <div className="experience-the">
+          <h3 className='exp-h'>Experience The <br /> Best Beauty</h3>
+          <p>Services</p>
+          </div>
+          <div className="variants-txt">
+            <h4>They are many variants</h4>
+          </div>
+          <a href='#' className="appointement-cta">
+            <img src="./public/iicofontcartalt.svg" alt="" />
+            <p>make an appointement</p>
+          </a>
+        </div>
+        <div className="right-sec">
+        <img src="./public/layerpng1@2x.png" alt="" />
+      </div>
+      <img className="pseudo-icon" alt="" src="./public/pseudo@2x.png" />
+      
+      <div className="divround-anim" />
+      </div>
+      <div className="section2">
+        <div className="section-outline">
+          <p className="welcome">Welcome</p>
+         
+
+          <p ><b >An Increadible</b> <span>Spa Experience</span></p>
+          <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverramaecenas accumsan lacus vel facilisis.</h5>
+        </div>
+        <div className="imgs">
+          <img src="./public/divactionbox@3x.png" alt="" />
+          <img src="./public/divactionbox1@3x.png" alt="" />
+        </div>
+        
+      </div>
+    <div className="section3">
+      <div className="sec3-left">
+        <img className='mainsec3img' src="./public/3jpg1@2x.png" alt="" />
+        <img className='leftsec3img' src="/6png@2x.png" alt="" />
+        <div className="expCounter">
+          <h1 className="Yrs">20 +</h1>
+          <h2 className="YrsHL">Years of Experience</h2>
+        </div>
+      </div>
+      <div className="sec3-right">
+        <h1 className='Headline2'>Beauty and <span>Spa <br /> Center</span> <img src="/pseudo1@2x.png" alt="" /></h1>
+        <h3>Lorem ipsum, dolor sit amet consectetur adipisicing.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates voluptatibus ullam incidunt laudantium atque enim ipsam quae maxime sit eaque recusandae, velit quaerat repellendus delectus aperiam expedita, architecto assumenda quidem!</p>
+        <a href='#' className="link">
+          <img src="/iicofontlongarrowright.svg" alt="" />
+          <p>learn more</p>
+        </a>
+      </div>
+    </div>
+    <div className="section4">
+      <div className="section-outline">
+            <p className="welcome">What We Do</p>
+          
+
+            <p ><b >Our</b> <span>Services</span></p>
+            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverramaecenas accumsan lacus vel facilisis.</h5>
+      </div>
+      <div className="serviceItem">
+        <div className="icons">
+          <img className='mainIcon' src="/icon.svg" alt="" />
+          <img className='secIcon' src="/icon1.svg" alt="" />
+          <h1>Stone Spa</h1>
+          <p>Risus commodo viverramaecenas accumsan lacus velfacilisis.</p>
+        </div>
+        <div className="icons">
+          <img className='mainIcon' src="/icon2.svg" alt="" />
+          <img className='secIcon' src="/icon3.svg" alt="" />
+          <h1>Candle Message</h1>
+          <p>Risus commodo viverramaecenas accumsan lacus velfacilisis.</p>
+        </div>
+        <div className="icons">
+          <img className='mainIcon' src="/icon4.svg" alt="" />
+          <img className='secIcon' src="/icon5.svg" alt="" />
+          <h1>Mortar</h1>
+          <p>Risus commodo viverramaecenas accumsan lacus velfacilisis.</p>
+        </div>
+        <div className="icons">
+          <img className='mainIcon' src="/icon6.svg" alt="" />
+          <img className='secIcon' src="/icon7.svg" alt="" />
+          <h1>Medicine</h1>
+          <p>Risus commodo viverramaecenas accumsan lacus velfacilisis.</p>
+        </div>
+      </div>
+    </div>
+    
+    {/* <div className="section5">
+      <div className="main-sec5">
+        <div className="appointement-form">
+          <h1>Make Appointement</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+        </div>
+        <form action="">
+          <input type="text" value="name" id="" />
+          <input type="email" value="E-mail" id="" />
+          <input className='date' type="date" value="select a date" id="" /> <br />
+          <textarea name="" id="" cols="45" rows="5"></textarea>
+        </form>
+        <a href="#"> <img className='cta-img' src="/iicofontcalendar.svg" alt="" /> <p>make ann appointement</p></a>
+      </div>
+    </div> */}
+      <div className="section6">
+        <img src="/1png1@2x.png" alt="" />
+        <img src="/2png2@2x.png" alt="" />
+        <img src="/3png@2x.png" alt="" />
+        <img src="/4png1@2x.png" alt="" />
+        <img src="/5png1@2x.png" alt="" />
+      </div>
+      <div className="section9">
+        <div className="sec9-info">
+          <img class="quotepng-icon" alt="" src="/quotepng@2x.png"/>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmodtempor <br /> incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices</p>
+          <div className="author">
+            <p>Mark Smith</p>
+            <p>London, UK</p>
+          </div>
+        </div>
+        <div className="authors-imgs">
+          <img src="/t1png@2x.png" alt="" />
+          <img src="/t3png@2x.png" alt="" />
+          <img src="/t2png@2x.png" alt="" />
+        </div>
+      </div>
+      <div className="section10">
+        <div className="section-outline10">
+              <h6 className="welcome">Get Inspiration</h6>
+            
+
+              <p ><b >price plans</b> <span>Services</span></p>
+              <h4 className='outline-h4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverramaecenas accumsan lacus vel facilisis.</h4>
+        </div>
+        <div className="row">
+          <div className="row-inspiration">
+            <img src="/p1jpg@2x.png" alt="" />
+            <div className="info">
+              <div className="headline">
+                <h1>Makeup & Massage</h1>
+                <span>____________________ From</span>
+                <h1>50$</h1>
+              </div>
+              <p>20 mins Revitalize Facial</p>
+            </div>
+          </div>
+          <div className="row-inspiration">
+            <img src="/p2jpg@2x.png" alt="" />
+            <div className="info">
+              <div className="headline">
+                <h1>Makeup & Massage</h1>
+                <span>____________________ From</span>
+                <h1>50$</h1>
+              </div>
+              <p>20 mins Revitalize Facial</p>
+            </div>
+          </div>
+          <div className="row-inspiration">
+            <img src="/p3jpg@2x.png" alt="" />
+            <div className="info">
+              <div className="headline">
+                <h1>Makeup & Massage</h1>
+                <span>____________________ From</span>
+                <h1>50$</h1>
+              </div>
+              <p>20 mins Revitalize Facial</p>
+            </div>
+          </div>
+          <div className="row-inspiration">
+            <img src="/p4jpg@2x.png" alt="" />
+            <div className="info">
+              <div className="headline">
+                <h1>Makeup & Massage</h1>
+                <span>____________________ From</span>
+                <h1>50$</h1>
+              </div>
+              <p>20 mins Revitalize Facial</p>
+            </div>
+          </div>
+          <div className="row-inspiration">
+            <img src="/p5jpg@2x.png" alt="" />
+            <div className="info">
+              <div className="headline">
+                <h1>Makeup & Massage</h1>
+                <span>____________________ From</span>
+                <h1>50$</h1>
+              </div>
+              <p>20 mins Revitalize Facial</p>
+            </div>
+          </div>
+          <div className="row-inspiration">
+            <img src="/p6jpg@2x.png" alt="" />
+            <div className="info">
+              <div className="headline">
+                <h1>Makeup & Massage</h1>
+                <span>____________________ From</span>
+                <h1>50$</h1>
+              </div>
+              <p>20 mins Revitalize Facial</p>
+            </div>
+          </div>
+          <div className="row-inspiration">
+            <img src="/p7jpg@2x.png" alt="" />
+            <div className="info">
+              <div className="headline">
+                <h1>Makeup & Massage</h1>
+                <span>____________________ From</span>
+                <h1>50$</h1>
+              </div>
+              <p>20 mins Revitalize Facial</p>
+            </div>
+          </div>
+          <div className="row-inspiration">
+            <img src="/p8jpg@2x.png" alt="" />
+            <div className="info">
+              <div className="headline">
+                <h1>Makeup & Massage</h1>
+                <span>____________________ From</span>
+                <h1>50$</h1>
+              </div>
+              <p>20 mins Revitalize Facial</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="section8">
+        <div className="section-outline">
+              <p className="welcome">News About Our Company</p>
+            
+
+              <p ><b >News </b> <span>Feed</span></p>
+              <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverramaecenas accumsan lacus vel facilisis.</h5>
+        </div>
+        <div className="newsContainer">
+          <div className="newsPost">
+            <img src="/1jpg1@2x.png" alt="" />
+            <div className="newsCard">
+              <p className='card-title'>February 18, 2017</p>
+              <p className='card-infos'>Spring is in the Air and and So Our These Amazing Spa Offers</p>
+              <a href='#' className="appointement-cta2">Learn More</a>
+            </div>
+          </div>
+          <div className="newsPost">
+            <img src="/1jpg2@2x.png" alt="" />
+            <div className="newsCard">
+              <p className='card-title'>February 18, 2017</p>
+              <p className='card-infos'>Spring is in the Air and and So Our These Amazing Spa Offers</p>
+              <a href='#' className="appointement-cta2">Learn More</a>
+            </div>
+          </div>
+          <div className="newsPost">
+            <img src="/3jpg@2x.png" alt="" />
+            <div className="newsCard">
+              <p className='card-title'>February 18, 2017</p>
+              <p className='card-infos'>Spring is in the Air and and So Our These Amazing Spa Offers</p>
+              <a href='#' className="appointement-cta2">Learn More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="section7">
+        <img src="/3jpg1@2x.png" alt="" />
+        <img src="/1jpg1@2x.png" alt="" />
+        <img src="/1jpg2@2x.png" alt="" />
+        <img src="/2jpg@2x.png" alt="" />
+        <img src="/3jpg@2x.png" alt="" />
+      </div>
+      <div className="footer">
+        <div className="about">
+          <h1>About Us</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipi-scing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href='#' className="appointement-cta">
+            <img src="./public/iicofontcartalt.svg" alt="" />
+            <p>make an appointement</p>
+          </a>
+        </div>
+        <div className="newsLetter">
+          <h1>news Letter</h1>
+          <p>Enter your email address to get latest updates and offers from us.</p>
+          <form action="">
+            <input type="email" value="enter your E-mail" id="" />
+            <img src="/button.svg" alt="" />
+          </form>
+        </div>
+        <div className="contact">
+          <div className="contactElement">
+            <img src="/iicofontlocationpin1.svg" alt="" />
+            <div>
+              <p>72 Main Drive,</p>
+              <p>Calibry, Florida 20304</p>
+            </div>
+          </div>
+          <div className="contactElement">
+            <img src="/iicofontphone.svg" alt="" />
+            <div>
+              <p>Helpline 24/7</p>
+              <p>+1 (700) 111 00 222</p>
+            </div>
+            
+          </div>
+          <div className="contactElement">
+            <img src="/iicofontclocktime1.svg" alt="" />
+            <div>
+              <p>Mon to Friday</p>
+              <p>9:00 am to 7:00 pm</p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <div className="copyright">
+        <p>© 2021 ThemeWar . All Rights Reserved.</p>
+        <img class="divcopy-social-icon" alt="" src="/divcopy-social.svg"/>
+      </div>
+    </div>
+  )
+}
+
+export default App
